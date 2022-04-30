3ee167ea4a49f67010ba4e0100e3e00105c62c90
Alihassoon93 <dmatthew.1993@gmail.com>
2022-05-01 00:47:19 +0300

    travel app

---
+++ src/components/Hero.jsx
@@ -0,0 +1,55 @@
+import { React, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import beachVid from "../assets/beachVid.mp4";
+
+const Hero = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`You Searched For: ${search}`);
+  };
+  return (
+    <div className=" w-full h-screen relative">
+      <video
+        className=" w-full h-full object-cover"
+        src={beachVid}
+        autoPlay
+        loop
+        muted
+      />
+      {/* overlay separator div */}
+      <div className=" absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
+      <div className=" absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
+        <h1>First Class Travel</h1>
+        <h2 className=" py-4">Top 1% Locations Worldwide</h2>
+
+        <form
+          onSubmit={handleSubmit}
+          className=" flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90"
+        >
+          <div>
+            <input
+              className=" bg-transparent w-[300px] sm:w-[400px] font-[poppins] focus:outline-none"
+              type="text"
+              placeholder="Search Destinations"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <div>
+            <button>
+              <AiOutlineSearch
+                size={20}
+                className="icon"
+                style={{ color: "#ffffff" }}
+              />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
